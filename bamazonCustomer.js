7ddf1a1e806170e58f42993c534b62383d344360
William Homer <William Homer>
2017-08-06 15:57:46 -0500

    pusedocoding out the next two features

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,14 +16,14 @@ function displayItems(){
 	var query = 'SELECT item_id, product_name, department_name, price, stock_qty FROM products'
 	connection.query(query, function(err, items){
 		//display all the details to the page
-		console.log(items);
+		//console.log(items);
 		//cycle through the items and display each item
 		//console.log('id		Product					department			price				qty')
 		for (var i =0; i < items.length; i++){
 			var print = items[i].item_id +'	';
-			print += items[i].product_name +'	';
-			print += items[i].department_name +'	';
-			print += items[i].price +'	';
+			print += items[i].product_name +'		';
+			print += items[i].department_name +'		';
+			print += items[i].price +'		';
 			print += items[i].stock_qty;
 			console.log(print);
 		}
@@ -31,17 +31,31 @@ function displayItems(){
 	connection.end();
 }
 
-connection.connect(function(err) {
-	var newProduct = {
-		productName: 'Lawn Mower',
-		departmentName: 'Landscaping',
-		price: 1200.99,
-		stockQty: 500
-	}
-	//createProduct(newProduct);
-	displayItems();
-})
+// connection.connect(function(err) {
+// 	var newProduct = {
+// 		productName: 'Lawn Mower',
+// 		departmentName: 'Landscaping',
+// 		price: 1200.99,
+// 		stockQty: 500
+// 	}
+// 	//createProduct(newProduct);
+// 	//displayItems();
+// })
 
+//prompt for user input to determine what they want to do
+function userPrompt(){
+	//ask the user what they would like to buy (by id)
+
+	//ask the user how many they would like to buy
+}
+
+//function to sell the items by completing the order
+function sellItems(itemID, qtyRequested){
+	//check qty available by itemID and compare qty requested
+		//if equal to or less than process order and display cost
+
+		//if greater than stock qty display error message
+}
 
 function createProduct(newProduct){
   var query = connection.query(
@@ -55,3 +69,8 @@ function createProduct(newProduct){
 	)
 	connection.end();
 }
+
+
+
+displayItems();
+userPrompt();
